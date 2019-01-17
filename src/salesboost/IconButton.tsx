8d638172ffe19241b9c0021aea311d322256f94e
minidonut <nycom13@gmail.com>
2019-01-17 17:53:45 +0900

    [update] typing: interface, types for core components

---
+++ src/salesboost/IconButton.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styled from "styled-components";
+import {
+  space,
+  SpaceProps
+} from "styled-system";
+import {
+  Icon
+} from "../core";
+
+
+export const IconButton = (props) => {
+  return (
+    <div></div>
+  );
+}
