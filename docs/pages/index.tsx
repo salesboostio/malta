7a32057c3a2fec72382616f00f7012284efc6b68
minidonut <nycom13@gmail.com>
2019-02-14 21:21:19 +0900

    drawer/add: ServiceDrawer

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,21 +1,39 @@
 import * as React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import {
-  theme
+  theme,
+  Header,
+  IconButton,
 } from "@salesboost/malta";
 import {
   getAPI,
   getCatalogURL,
   getAuthURL,
 } from "@salesboost/common";
+import {
+  ServiceDrawer,
+  SettingDrawer,
+} from "@salesboost/drawer";
 
 class Index extends React.Component<any, any> {
 
+  renderServiceDrawer = (iconColor = "rgba(22,27,72,0.8)") =>
+    <ServiceDrawer
+      opener={({ handleOpen }) => <IconButton name="hamburger" onClick={handleOpen as any} ml={2} color={iconColor} />} />
+
+  renderSettingDrawer = (iconColor = "rgba(22,27,72,0.8)") =>
+    <SettingDrawer opener={({ handleOpen }) => <IconButton name="setting" onClick={handleOpen} mr={3} ml="auto" color={iconColor} />}
+      userIndex={"0"} />;
+
+
   render() {
-    console.log(getAPI(), getCatalogURL(), getAuthURL());
     return (
       <ThemeProvider theme={theme}>
         <div style={{ paddingTop: "60px" }}>
+          <Header>
+            {this.renderServiceDrawer()}
+            {this.renderSettingDrawer()}
+          </Header>
           asdf
         </div>
       </ThemeProvider>
