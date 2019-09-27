773c1c7ff6a6b0c1dc18c7bdbd0d898822a7a28e
Cielo <buen_cielo@naver.com>
2019-09-27 15:44:25 +0900

    [Refactor] Fix: convert hard coding value to Theme font-weight.
    #TODO
    1. Text 를 감싸는 Block(div) 과 Text 자체의 속성 분리 필요가 필요 해보입니다.
    : 단단하게 묶여잇는 div tag와 p tag속성을 분리시켜 따로 관리하는것이 좋을것같습니다.

--- src/core/atom/Text.tsx
+++ src/core/atom/Text.tsx
@@ -15,7 +15,7 @@ import {
 } from "styled-system";
 import { Block, BlockProps } from "./Block";
 import { theme } from "../../salesboost";
-import { parseNumberToUnit } from "../../utils";
+import { parseNumberToUnit, setCssDeclaration } from "../../utils";
 
 interface Props {
   medium?: boolean;
@@ -31,17 +31,17 @@ interface Props {
   whiteSpace?: "normal" | "nowrap" | "pre" | "pre-wrap" | "pre-line";
 }
 
-const medium = ({ medium }: Props) => medium ? `font-weight: 500;` : "";
-const regular = ({ regular }: Props) => regular ? `font-weight: 400;` : "";
-const demiLight = ({ demiLight }: Props) => demiLight ? `font-weight: 300;` : "";
-const light = ({ light }: Props) => light ? `font-weight: 200;` : "";
-const alignLeft = ({ alignLeft }: Props) => alignLeft ? `text-align: left;` : "";
-const alignRight = ({ alignRight }: Props) => alignRight ? `text-align: right;` : "";
-const alignCenter = ({ alignCenter }: Props) => alignCenter ? `text-align: center;` : "";
-const overflowWrap = ({ overflowWrap }: Props) => overflowWrap ? `overflow-wrap: ${overflowWrap};` : "";
-const textOverflow = ({ textOverflow }: Props) => textOverflow ? `text-overflow: ${textOverflow};` : "";
-const wordBreak = ({ wordBreak }: Props) => wordBreak ? `word-break: ${wordBreak};` : "";
-const whiteSpace = ({ whiteSpace }: Props) => whiteSpace ? `white-space: ${whiteSpace};` : "";
+const medium = ({ medium }: Props) => medium && setCssDeclaration("font-weight", theme.fontweights.medium);
+const regular = ({ regular }: Props) => regular && setCssDeclaration("font-weight", theme.fontweights.regular);
+const demiLight = ({ demiLight }: Props) => demiLight && setCssDeclaration("font-weight", theme.fontweights.demilight);
+const light = ({ light }: Props) => light && setCssDeclaration("font-weight", theme.fontweights.light);
+const alignLeft = ({ alignLeft }: Props) => alignLeft && `text-align: left;`;
+const alignRight = ({ alignRight }: Props) => alignRight && `text-align: right;`;
+const alignCenter = ({ alignCenter }: Props) => alignCenter && `text-align: center;`;
+const overflowWrap = ({ overflowWrap }: Props) => overflowWrap && `overflow-wrap: ${overflowWrap};`;
+const textOverflow = ({ textOverflow }: Props) => textOverflow && `text-overflow: ${textOverflow};`;
+const wordBreak = ({ wordBreak }: Props) => wordBreak && `word-break: ${wordBreak};`;
+const whiteSpace = ({ whiteSpace }: Props) => whiteSpace && `white-space: ${whiteSpace};`;
 
 type InnerProps = Props & FontFamilyProps & FontSizeProps & TextAlignProps & FontWeightProps & LetterSpacingProps & LineHeightProps;
 
