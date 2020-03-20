ed610f49ecbb52f5a205218a563f3dcb41b40b4c
minidonut <nycom13@gmail.com>
2020-03-21 05:38:44 +0900

    Use eslint. not tslint(deprecated)

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    // There needs to declare underscore variables which is not used but only be destructured.
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    // disable rules below
+    "react/prop-types": 0,
+    "react/display-name": 0,
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/no-unused-vars": 0, // duplicated with typescript compiler
+    "@typescript-eslint/no-var-requires": 0, // allow require
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/no-use-before-define": 0, // TODO. consider apply this rule
+    "@typescript-eslint/explicit-function-return-type": 0,
+  },
+  settings: {
+    "react": {
+      "version": "detect",
+    },
+  },
+};
